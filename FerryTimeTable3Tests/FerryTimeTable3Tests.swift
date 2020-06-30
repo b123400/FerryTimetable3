bc7a8130d885bcf5bf22b6cc3c5eb6e5a2686ad2
b123400 <i@b123400.net>
2020-06-30 22:51:34 +0900

    Initial Commit

---
+++ FerryTimeTable3Tests/FerryTimeTable3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FerryTimeTable3Tests.swift
+//  FerryTimeTable3Tests
+//
+//  Created by b123400 on 2020/06/30.
+//  Copyright © 2020 b123400. All rights reserved.
+//
+
+import XCTest
+@testable import FerryTimeTable3
+
+class FerryTimeTable3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
